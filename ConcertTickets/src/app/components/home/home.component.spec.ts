d9122faa41582702a56dd56d1243998341153cda
AlfonsoTorrez <alftorres@csumb.edu>
2018-12-01 10:02:56 -0800

    Created the layout for concerts page and home page

--- ConcertTickets/src/app/components/home/home.component.spec.ts
+++ ConcertTickets/src/app/components/home/home.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
 
+
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
