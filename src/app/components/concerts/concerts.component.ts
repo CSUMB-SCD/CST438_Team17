bf6ec3237258c497dfb55989eeff651c9e6422fb
Ariel-McCarthy <mccarthy@Ariels-MacBook-Air.local>
2018-12-02 14:59:58 -0800

    fixing up concerts page

--- src/app/components/concerts/concerts.component.ts
+++ src/app/components/concerts/concerts.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { SquaddataService } from '../../services/squaddata.service';
 
 @Component({
   selector: 'app-concerts',
   templateUrl: './concerts.component.html',
   styleUrls: ['./concerts.component.scss']
 })
-export class ConcertsComponent implements OnInit {
-
-  constructor() { }
+export class CheckoutComponent implements OnInit {
+  tick$: {};
+  ticket$: {
+  artist: string;
+  venue: string;
+  date: string;
+  quantity: number;
+  price: number;
+  };
+  
+  constructor(private squadService: SquaddataService) {}
 
   ngOnInit() {
+    // this.show = [this.title, this.venue, this.date];
+    // this.tickets = [this.quantity, this.price];
+
+    this.squadService.getSquad().subscribe(
+      squadService => this.tick$ = squadService
+    );
   }
 
 }
