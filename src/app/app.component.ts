dd8c07969ec959b6ee1cbd2b88e616d0d26cd405
seanquevedo <squevedo@csumb.edu>
2018-12-04 16:44:51 -0800

    Revert "Merge pull request #40 from CSUMB-SCD/Austin-Feature"
    This reverts commit ddb8d94400ebcb32d893139e77f85b9b30128715, reversing
    changes made to 58af69a8cf8488e820d0d3d70f27f6b1b2a861c9.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { AppService } from './app.service';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+
 import 'rxjs/add/operator/finally';
+
 
 @Component({
   selector: 'app-root',
@@ -19,5 +21,6 @@ export class AppComponent {
           this.router.navigateByUrl('/signin');
       }).subscribe();
     }
+
 
 }
