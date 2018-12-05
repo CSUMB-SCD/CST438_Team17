17218cc3ce8f4256eec91c0bc6a104ea2b1c5f26
Austin Gray <austin.gray64@gmail.com>
2018-12-04 17:01:19 -0800

    Revert "Merge pull request #40 from CSUMB-SCD/Austin-Feature"
    This reverts commit ddb8d94400ebcb32d893139e77f85b9b30128715, reversing
    changes made to 58af69a8cf8488e820d0d3d70f27f6b1b2a861c9.

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AppService } from 'src/app/app.service';
-import { SquaddataService } from '../../services/squaddata.service';
 
 @Component({
   templateUrl: './signin.component.html'
@@ -10,15 +9,15 @@ import { SquaddataService } from '../../services/squaddata.service';
 export class SigninComponent {
 
   credentials = {username: '', password: ''};
-  user = {};
 
-  constructor(private squadService: SquaddataService, private router: Router) {
+  constructor(private app: AppService, private http: HttpClient, private router: Router) {
   }
 
   login() {
-      this.squadService.getUsers().subscribe(
-        squadService => this.user = squadService
-      );
+    this.app.authenticate(this.credentials, () => {
+        this.router.navigateByUrl('home');
+    });
+    return false;
   }
 
 }
