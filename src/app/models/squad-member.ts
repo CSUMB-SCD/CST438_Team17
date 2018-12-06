93c774dce2d673694872b86f20be14eb27c9c451
Ariel-McCarthy <mccarthy@Ariels-MacBook-Air.local>
2018-12-06 13:16:00 -0800

    fixed output

--- src/app/models/squad-member.ts
+++ src/app/models/squad-member.ts
@@ -1,2 +1,8 @@
 export class SquadMember {
+        artist: string;
+        venue: string;
+        date: string;
+        quantity: number;
+        price: number;
+        description: string;
 }
