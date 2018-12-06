b4c7e8c5d37a84cea54826a6807c0e98f939c1db
Austin Gray <austin.gray64@gmail.com>
2018-12-06 15:12:37 -0800

    Update checkout.component.ts

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -35,7 +35,7 @@ export class CheckoutComponent implements OnInit {
   ngOnInit() {
     // this.show = [this.title, this.venue, this.date];
     // this.tickets = [this.quantity, this.price];
-    // this.app.currentMessage.subscribe(message => this.message = message);
+     this.app.currentMessage.subscribe(message => this.message = message);
   if (this.message === 'x') {
       this.router.navigate(['../signin']);
     }
