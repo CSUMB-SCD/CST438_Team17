183cc972c8bdb819b8441008bfbffe294612260e
AlfonsoTorrez <alftorres@csumb.edu>
2018-12-06 13:21:51 -0800

    Working on it

--- src/app/services/ticket.service.ts
+++ src/app/services/ticket.service.ts
@@ -1,14 +1,21 @@
 import { Ticket } from './../models/ticket';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { OnInit} from '@angular/core';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TicketService {
+  private tickets: string[] = [];
+  constructor() {
+    this.tickets.push('Eminem', 'Green Day', 'Dillon Francis', 'Twenty One Pilots', 'The Strokes', 'Boy Pablo', 'Kendrick Lamar', 'Drake');
+   }
 
-  constructor() { }
+   getTickets(): String[] {
+    return this.tickets;
+  }
   // TO-DO attach heroku link to ticketssvc here
   // private baseURL = 'herokuapp.com/whatever';
   // private baseURL = 'https://team17-ticketservice.herokuapp.com/ConcertTickets';
